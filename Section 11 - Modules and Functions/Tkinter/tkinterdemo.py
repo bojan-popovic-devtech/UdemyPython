b4ae5cd706a1fc10e839e4ae9a7052f7e3b7a0cc
Bojan Popovic <bojan.popovic@devtechgroup.com>
2019-08-19 23:05:35 +0200

    added tkinter demo and gui

--- Section 11 - Modules and Functions/Tkinter/tkinterdemo.py
+++ Section 11 - Modules and Functions/Tkinter/tkinterdemo.py
@@ -2,8 +2,6 @@ try:
     import tkinter
 except ImportError: #python 2
     import Tkinter as tkinter
-print(tkinter.TkVersion)
-print(tkinter.TclVersion)
 
 #tkinter._test()
 
@@ -12,4 +10,23 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Hello World")
 mainWindow.geometry('640x480+8+400')
 
+label = tkinter.Label(mainWindow, text="hell owlrd")
+label.pack(side='top')
+
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.pack(side='top')
+
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.pack(side='right', anchor='n', expand=True)
+
+button1 = tkinter.Button(rightFrame, text='button1')
+button2 = tkinter.Button(rightFrame, text='button2')
+button3 = tkinter.Button(rightFrame, text='button3')
+button1.pack(side='top')
+button2.pack(side='top')
+button3.pack(side='top')
 mainWindow.mainloop()
+
